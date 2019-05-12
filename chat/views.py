86bb70d74e7726b8f1133f542aaa487bd99b4478
raeesaroj <raesaroj16@gmail.com>
2019-05-12 14:34:02 +0545

    room chat using django channels, wesocket and redis

---
+++ chat/views.py
@@ -0,0 +1,14 @@
+import json
+
+from django.utils.safestring import mark_safe
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'chat/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
